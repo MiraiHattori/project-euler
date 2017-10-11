21a346a3d48b81520e6f5f62d598eba54505f293
Mirai Hattori(2015) <mirai2045@gmail.com>
2017-10-11 11:07:51 +0900

    add 43

--- 3_43_sub_string_divisibility.cpp
+++ 3_43_sub_string_divisibility.cpp
@@ -1,5 +1,7 @@
+#include <algorithm>
 #include <array>
 #include <iostream>
+#include <list>
 #include <vector>
 
 /*
@@ -25,28 +27,57 @@ int getDigitNumDec(int num, int digit)
     return (num % power(10, digit)) / power(10, digit - 1);
 }
 
+
+std::vector<int> numList(int digit, int divisor, std::vector<int> temp_list)
+{
+    // 結果の代入
+    std::vector<int> result_list;
+    for (int temp : temp_list) {
+        std::list<int> digit_num_list = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < digit; i++) {
+            digit_num_list.remove(getDigitNumDec(temp, i + 1));
+        }
+        for (int digit_num : digit_num_list) {
+            int candidate = digit_num * power(10, 2) + getDigitNumDec(temp, digit) * 10 + getDigitNumDec(temp, digit - 1);
+            if (candidate % divisor == 0) {
+                result_list.emplace_back(digit_num * power(10, digit) + temp);
+            }
+        }
+    }
+    return result_list;
+}
+
 int main()
 {
     // 下3桁
     std::vector<int> temp_17list;
     for (int i = 0; i * 17 < 1000; i++) {
         std::array<int, 3> digits;
-        digits.at(2) = getDigitNumDec(i * 17, 3);
-        digits.at(1) = getDigitNumDec(i * 17, 2);
-        digits.at(0) = getDigitNumDec(i * 17, 1);
+        for (int j = 0; j < 3; j++) {
+            digits.at(j) = getDigitNumDec(i * 17, j + 1);
+        }
         if (digits.at(0) != digits.at(1) and digits.at(1) != digits.at(2) and digits.at(2) != digits.at(0)) {
             temp_17list.emplace_back(i * 17);
         }
     }
-    std::vector<int> temp_13list;
-    for (int i : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}) {
-        for (int temp_13 : temp_13list) {
-            int temp = i * 100 + getDigitNumDec(temp_17, 3) * 10
-                       + getDigitNumDec(temp_17, 2);
-            if (temp % 13 == 0) {
-                std::cout << i * 1000 + temp_17 << std::endl;
-            }
+    std::vector<int> temp_13list = numList(3, 13, temp_17list);
+    std::vector<int> temp_11list = numList(4, 11, temp_13list);
+    std::vector<int> temp_7list = numList(5, 7, temp_11list);
+    std::vector<int> temp_5list = numList(6, 5, temp_7list);
+    std::vector<int> temp_3list = numList(7, 3, temp_5list);
+    std::vector<int> temp_2list = numList(8, 2, temp_3list);
+    std::sort(temp_2list.begin(), temp_2list.end(), [](int a, int b) { return a > b; });
+    long long answer = 0;
+    for (int temp : temp_2list) {
+        std::list<int> digit_num_list = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < 9; i++) {
+            digit_num_list.remove(getDigitNumDec(temp, i + 1));
+        }
+        for (int digit_num : digit_num_list) {
+            answer += static_cast<long long>(digit_num) * static_cast<long long>(power(10, 9)) + static_cast<long long>(temp);
         }
     }
+    std::cout << "The answer is " << answer << "." << std::endl;
+
     return 0;
 }
